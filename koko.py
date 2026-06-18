b2baf5532270f5522f9a0b5008290c4d0423fae6
MrKizo <kareemmohamedgamal42@gmail.com>
2026-06-18 22:42:47 +0300

    18/6

--- koko.py
+++ koko.py
@@ -7,3 +7,21 @@ print (2 == 4)
 myvariable = 10 
 print (myvariable) 
 
+Age = 23   
+print (Age) 
+Name = "Kizo"
+print (Name) 
+Myname = "kareem"
+print (Myname)
+My_name = "Kimo"
+print (My_name)
+
+print (type (Name))
+
+x = 10  
+
+x= "hello" 
+
+print (x)
+
+help("keywords")
